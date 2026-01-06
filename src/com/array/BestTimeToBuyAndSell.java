abb2b0579f0aa6265405335be258a26a0abe91d0
Kaif <kaifmumtajansari@gmail.com>
2026-01-06 12:12:02 +0530

    Third Program

--- src/com/array/BestTimeToBuyAndSell.java
+++ src/com/array/BestTimeToBuyAndSell.java
@@ -1,4 +1,20 @@
 package com.array;
 
 public class BestTimeToBuyAndSell {
+    public static int maxProfit(int[] nums) {
+        int profit = 0;
+        int buy = nums[0];
+        for (int num : nums) {
+            if (buy > num) {
+                buy = num;
+            } else if (num - buy > profit) {
+                profit = num - buy;
+            }
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int[] nums = {7,1,5,3,6,4};
+        System.out.println(maxProfit(nums));
+    }
 }
