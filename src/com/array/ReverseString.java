b445aec9f6e31769fb4c7ea5b38ee64a4712c97e
Kaif <kaifmumtajansari@gmail.com>
2026-01-08 08:35:11 +0530

    ReverseString

---
+++ src/com/array/ReverseString.java
@@ -0,0 +1,15 @@
+package com.array;
+
+public class ReverseString {
+    public static void reverse(String str){
+        String result = "";
+        for(int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        String str = "kaif";
+        reverse(str);
+    }
+}
