f9055850617e9e1ba7d0969d4376e3d968cf20eb
Kaif <kaifmumtajansari@gmail.com>
2026-01-09 11:19:58 +0530

    Longest Common Prefix

--- src/com/array/string/LongestCommonPrefix.java
+++ src/com/array/string/LongestCommonPrefix.java
@@ -2,6 +2,7 @@ package com.array.string;
 
 public class LongestCommonPrefix {
 
+
     // Brute force Approach
     public static String longestCommonPrefix(String[] strs){
         int n = strs.length;
@@ -18,8 +19,23 @@ public class LongestCommonPrefix {
         }
         return first;
     }
+
+    // Optimal
+
+    public static String lcp(String[] strs){
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++){
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0,prefix.length() - 1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
     public static void main(String[] args) {
         String[] strs = {"alower","aow","aosk"};
         System.out.println(longestCommonPrefix(strs));
+        System.out.println(lcp(strs));
     }
 }
