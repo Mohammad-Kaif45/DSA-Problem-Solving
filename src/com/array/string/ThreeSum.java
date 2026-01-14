6a7028a0ebdcd672e5ab5e178c8a175c6c13c410
Kaif <kaifmumtajansari@gmail.com>
2026-01-14 11:25:41 +0530

    Add Optimal Solution

--- src/com/array/string/ThreeSum.java
+++ src/com/array/string/ThreeSum.java
@@ -20,9 +20,39 @@ public class ThreeSum {
         List<List<Integer>> rersult = new ArrayList<>(set);
         return rersult;
     }
+
+    // Optimal Approach
+
+    public static List<List<Integer>> triplet(int[] arr) {
+        int n = arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        for(int i = 0; i < n; i++) {
+            if(i != 0 && arr[i] == arr[i - 1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if(sum < 0) {
+                    j++;
+                }else if(sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = Arrays.asList(arr[i],arr[j],arr[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while (j < k && arr[j] == arr[j - 1]) j++;
+                    while (j < k && arr[k] == arr[k + 1]) k--;
+                }
+            }
+        }
+        return ans;
+    } // Time complexity :  O(NlogN)+O(N2)
     public static void main(String[] args) {
         int[] nums = {-2,-2,-2,-1,-1,-1,0,0,0,2,2,2,2};
-        List<List<Integer>> ans = threeSum(nums);
+        //List<List<Integer>> ans = threeSum(nums);
+        List<List<Integer>> ans = triplet(nums);
         for (List<Integer> it : ans) {
             System.out.print("[");
             for (Integer i : it) {
