6f0428b500b59d3eb11df14eede2dfe5101868dd
Kaif <kaifmumtajansari@gmail.com>
2026-01-15 11:52:40 +0530

    MaxAreaOfSquareHole.java

---
+++ src/com/array/string/MaxAreaOfSquareHole.java
@@ -0,0 +1,33 @@
+package com.array.string;
+
+import java.util.Arrays;
+
+public class MaxAreaOfSquareHole {
+    public static int maxArea(int n,int m,int[] hBars,int[] vBars){
+        int height = getMaxGap(hBars);
+        int width = getMaxGap(vBars);
+        int side = Math.min(height,width);
+        return side * side;
+    }
+
+    public static int getMaxGap(int[] bars){
+        Arrays.sort(bars);
+        int maxCount = 1;
+        int currentCount = 1;
+        for(int i = 1;i < bars.length; i++){
+            if(bars[i] == bars[i - 1] + 1){
+                currentCount++;
+            } else {
+                currentCount = 1;
+            }
+            maxCount = Math.max(maxCount,currentCount);
+        }
+        return maxCount + 1;
+    }
+    public static void main(String[] args) {
+        int n = 2, m = 1;
+        int[] hBars = {2,3};
+        int[] vBars = {2};
+        System.out.println(maxArea(n,m,hBars,vBars));
+    }
+}
