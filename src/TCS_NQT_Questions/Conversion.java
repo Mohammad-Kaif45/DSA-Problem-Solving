4b603e0f16014aa4615f3ffab31a75c6d212cfaa
Kaif <kaifmumtajansari@gmail.com>
2026-02-10 23:17:52 +0530

    Conversion

--- src/TCS_NQT_Questions/Conversion.java
+++ src/TCS_NQT_Questions/Conversion.java
@@ -1,4 +1,13 @@
 package TCS_NQT_Questions;
 
+import java.util.Scanner;
+
 public class Conversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int bitCount = (int)(Math.log(n) / Math.log(2)) + 1;
+        int mask = (1 << bitCount) - 1;
+        System.out.println(n ^ mask);
+    }
 }
